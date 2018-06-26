316f592890a9ba3489ca6f2723b0108b790ac68a
Jose M. Peña <josemanuel.pena@tecnotramit.com>
2018-06-26 09:51:28 +0200

    Parse and execute template

--- main.go
+++ main.go
@@ -2,15 +2,36 @@ package main
 
 import (
 	"net/http"
-	"fmt"
+	"log"
+	"time"
+	"html/template"
 )
 
-func helloWorld(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintf(w, "Hello World")
+type PageVariables struct {
+	Date string
+	Time string
+}
+
+func Index(w http.ResponseWriter, r *http.Request){
+	now := time.Now()
+	IndexVars := PageVariables{ // store date and time in s struct
+		Date: now.Format("2-7-2006"),
+		Time: now.Format("15:04:05"),
+	}
+
+	t, err := template.ParseFiles("./views/index.html")
+	if err != nil {
+		log.Print("template parsing error: ", err)
+	}
+
+	err = t.Execute(w, IndexVars)
+	if err != nil {
+		log.Print("template execution error: ", err)
+	}
 }
 
 
 func main() {
-	http.HandleFunc("/", helloWorld)
-	http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", Index)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
